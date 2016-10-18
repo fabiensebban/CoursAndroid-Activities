c65a21d2abdf38a6a5092a9b74e0b7e0a20cd1da
Fabian Sebban <fa.sebban@gmail.com>
2016-10-18 11:27:44 +0200

    Shared Preferences

---
+++ app/src/main/java/com/example/fabiansebban/coursandroid_activities/Constant.java
@@ -0,0 +1,10 @@
+package com.example.fabiansebban.coursandroid_activities;
+
+/**
+ * Created by fabiansebban on 18/10/16.
+ */
+public class Constant {
+    public static final int CALL_BACK_RESULT = 42;
+    public static final String SHARED_PREFERENCE_TEST = "SHARED_PREFERENCE_TEST";
+    public static final String SHARED_PREFERENCE_NAME = "SHARED_PREFERENCE_NAME";
+}
--- app/src/main/java/com/example/fabiansebban/coursandroid_activities/MainActivity.java
+++ app/src/main/java/com/example/fabiansebban/coursandroid_activities/MainActivity.java
@@ -1,18 +1,30 @@
 package com.example.fabiansebban.coursandroid_activities;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import com.example.fabiansebban.coursandroid_activities.SubActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button shareButton;
+    private Button otherLayoutButton;
+    private Button sharedPrefenrecesButton;
+    private String message;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Instance of the shared preference
+        SharedPreferences sharedPreferences = getSharedPreferences("con", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(Constant.SHARED_PREFERENCE_TEST, "testing shared preferences");
+        editor.commit();
 
         shareButton = (Button)findViewById(R.id.shareButton);
 
@@ -21,9 +33,39 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent sendIntent = new Intent();
                 sendIntent.setAction(Intent.ACTION_SEND);
-                sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to share");
+                sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my awwwwwsome text to share !! ");
                 sendIntent.setType("text/plain");
-                startActivity(sendIntent);
+                startActivityForResult(sendIntent, Constant.CALL_BACK_RESULT);
+            }
+        });
+
+        otherLayoutButton = (Button)findViewById(R.id.buttonToLayout);
+
+        otherLayoutButton.setOnClickListener(new  View.OnClickListener() {
+
+            public void onClick(View v) {
+                message = "test";
+
+                Intent intent = new Intent(MainActivity.this, SubActivity.class);
+                intent.putExtra(intent.EXTRA_TEXT, message);
+                startActivityForResult(intent, Constant.CALL_BACK_RESULT);
+            }
+        });
+
+        sharedPrefenrecesButton = (Button)findViewById(R.id.sharedPreferencesButton);
+
+        sharedPrefenrecesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Set SharedPreferences params
+                SharedPreferences sharedPreferences = getSharedPreferences(Constant.SHARED_PREFERENCE_NAME, MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString(Constant.SHARED_PREFERENCE_TEST, "testing shared preferences");
+                editor.commit();
+
+                //Open a new intent
+                Intent intent = new Intent(MainActivity.this, SubActivity.class);
+                startActivity(intent);
             }
         });
     }
